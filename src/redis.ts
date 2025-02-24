932aa128953cc41ecdc581b2e9733c1ccee73567
Nadeesha Cabral <n@nadeesha.me>
2025-02-25 07:00:11 +1100

    fix: Minor changes 2025-02-25-06:53:44

--- src/redis.ts
+++ src/redis.ts
@@ -3,4 +3,6 @@ import assert from "assert";
 
 assert(process.env.REDIS_URL, "REDIS_URL is required");
 
-export const redis = new Redis(process.env.REDIS_URL);
+export const redis = new Redis(process.env.REDIS_URL, {
+  family: 6,
+});
