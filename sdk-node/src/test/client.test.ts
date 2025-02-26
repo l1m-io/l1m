99a556d59bce2418ae740e33be1643d17bd5d672
John Smith <john@johnjcsmith.com>
2025-02-26 14:45:07 +1030

    feat: Wrap errors in SDK

--- sdk-node/src/test/client.test.ts
+++ sdk-node/src/test/client.test.ts
@@ -41,9 +41,42 @@ async function testCallStructuredZod() {
   assert.strictEqual((result as any).data.character, "Shrek");
 }
 
+async function testInvalidApiKey() {
+  const l1m = new L1M({
+    baseUrl: "http://localhost:3000",
+    provider: {
+      model: process.env.TEST_PROVIDER_MODEL!,
+      key: "INVALID",
+      url: process.env.TEST_PROVIDER_URL!,
+    }
+  });
+
+  const url = "https://upload.wikimedia.org/wikipedia/en/4/4d/Shrek_%28character%29.png";
+  const buffer =  await fetch(url).then((response) => response.arrayBuffer());
+  const input = Buffer.from(buffer).toString("base64");
+
+  const result = l1m.structured({
+    input,
+    schema: z.object({
+      character: z.string(),
+    })
+  })
+
+  assert.rejects(result, "Should fail with invalid API key");
+  const error = await result.catch((e) => e);
+
+  console.log("Result", {
+    error,
+  });
+
+  assert.strictEqual(error.statusCode, 401);
+  assert.strictEqual(error.message, "Failed to call provider");
+}
+
 // Main test runner - executes all tests
 (async function runAllTests() { console.log("Starting tests...");
-  await runTest("structured (zod)", testCallStructuredZod);
+  //await runTest("structured (zod)", testCallStructuredZod);
+  await runTest("invalid api key", testInvalidApiKey);
 
   console.log("All tests completed");
 })().catch(console.error);
